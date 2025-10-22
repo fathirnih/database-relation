f7511d1b663d88c6023a47233208925d7665509d
Fathir <fathirulfatih29@gmail.com>
2025-10-22 09:00:59 +0700

    add content siswacontroller

--- app/Http/Controllers/SiswaController.php
+++ app/Http/Controllers/SiswaController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Siswa;
 use Illuminate\Http\Request;
 
 class SiswaController extends Controller
 {
-    //
+    public function index()
+    {
+        // Ambil semua siswa dengan relasi ekskul
+        $siswas = Siswa::with('ekskuls')->get();
+
+        // Kirim ke view
+        return view('index', compact('siswas'));
+    }
 }
