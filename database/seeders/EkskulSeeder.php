25b96c1ae68ed9204ab1be61eab85281511abe29
Fathir <fathirulfatih29@gmail.com>
2025-10-22 09:05:28 +0700

    add content eksulceeder

--- database/seeders/EkskulSeeder.php
+++ database/seeders/EkskulSeeder.php
@@ -2,16 +2,15 @@
 
 namespace Database\Seeders;
 
-use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Ekskul;
 
 class EkskulSeeder extends Seeder
 {
-    /**
-     * Run the database seeds.
-     */
     public function run(): void
     {
-        //
+        Ekskul::create(['nama' => 'Basket']);
+        Ekskul::create(['nama' => 'Pramuka']);
+        Ekskul::create(['nama' => 'Paskibra']);
     }
 }
